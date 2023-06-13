190175b884d0952da872faeccfc1f48719ddd133
hlrmp <haysjhune@gmail.com>
2023-06-13 11:50:53 +0800

    s

--- trial/Form1.Designer.cs
+++ trial/Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             dataGridView1 = new DataGridView();
             button1 = new Button();
             button2 = new Button();
@@ -40,16 +42,26 @@
             dataGridView2 = new DataGridView();
             label3 = new Label();
             label4 = new Label();
+            textBox3 = new TextBox();
+            button5 = new Button();
+            dataGridView3 = new DataGridView();
+            comboBox1 = new ComboBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button6 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = Color.FromArgb(255, 192, 192);
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(330, 56);
+            dataGridView1.GridColor = Color.Cyan;
+            dataGridView1.Location = new Point(328, 82);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
             dataGridView1.RowTemplate.Height = 25;
@@ -58,7 +70,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(456, 248);
+            button1.Location = new Point(444, 272);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
@@ -111,12 +123,13 @@
             // 
             // button4
             // 
-            button4.Location = new Point(41, 248);
+            button4.Location = new Point(345, 272);
             button4.Name = "button4";
             button4.Size = new Size(75, 23);
             button4.TabIndex = 7;
             button4.Text = "edit";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // label2
             // 
@@ -131,8 +144,27 @@
             // 
             dataGridView2.AllowUserToAddRows = false;
             dataGridView2.AllowUserToDeleteRows = false;
+            dataGridView2.BackgroundColor = Color.FromArgb(192, 192, 255);
+            dataGridView2.CellBorderStyle = DataGridViewCellBorderStyle.SunkenVertical;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(192, 192, 255);
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(128, 128, 255);
+            dataGridViewCellStyle1.SelectionForeColor = Color.FromArgb(255, 192, 255);
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(604, 56);
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(192, 192, 255);
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(192, 255, 255);
+            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(64, 0, 64);
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dataGridView2.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView2.GridColor = Color.Red;
+            dataGridView2.Location = new Point(587, 82);
             dataGridView2.Name = "dataGridView2";
             dataGridView2.ReadOnly = true;
             dataGridView2.RowTemplate.Height = 25;
@@ -142,7 +174,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(432, 27);
+            label3.Location = new Point(424, 64);
             label3.Name = "label3";
             label3.Size = new Size(22, 15);
             label3.TabIndex = 10;
@@ -151,17 +183,88 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(684, 27);
+            label4.Location = new Point(659, 64);
             label4.Name = "label4";
             label4.Size = new Size(63, 15);
             label4.TabIndex = 11;
             label4.Text = "archive list";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(482, 15);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(240, 23);
+            textBox3.TabIndex = 12;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(728, 15);
+            button5.Name = "button5";
+            button5.Size = new Size(59, 23);
+            button5.TabIndex = 13;
+            button5.Text = "search";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.AllowUserToAddRows = false;
+            dataGridView3.AllowUserToDeleteRows = false;
+            dataGridView3.BackgroundColor = Color.WhiteSmoke;
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.GridColor = SystemColors.ActiveCaptionText;
+            dataGridView3.Location = new Point(328, 83);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.ReadOnly = true;
+            dataGridView3.RowTemplate.Height = 25;
+            dataGridView3.Size = new Size(240, 174);
+            dataGridView3.TabIndex = 14;
+            dataGridView3.Visible = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(587, 44);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(240, 23);
+            comboBox1.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(41, 219);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(162, 219);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 17;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(139, 268);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 18;
+            button6.Text = "log in";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(879, 329);
+            Controls.Add(button6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(comboBox1);
+            Controls.Add(dataGridView3);
+            Controls.Add(button5);
+            Controls.Add(textBox3);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(dataGridView2);
@@ -178,6 +281,7 @@
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -196,5 +300,12 @@
         private DataGridView dataGridView2;
         private Label label3;
         private Label label4;
+        private TextBox textBox3;
+        private Button button5;
+        private DataGridView dataGridView3;
+        private ComboBox comboBox1;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button6;
     }
 }
--- trial/Form1.cs
+++ trial/Form1.cs
@@ -5,6 +5,7 @@ using System.Windows.Forms;
 using static System.ComponentModel.Design.ObjectSelectorEditor;
 using System.Xml.Linq;
 using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace trial
 {
@@ -13,13 +14,14 @@ namespace trial
         public Form1()
         {
             InitializeComponent();
+            lb();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             see();
-            
-          
+
+
         }
         string conn = (@"Data Source=REICHEL28\SQLEXPRESS01;Initial Catalog=TBLTRIAL;Integrated Security=True");
 
@@ -83,7 +85,7 @@ namespace trial
 
 
         }
-     
+
 
         public void add()
         {
@@ -95,15 +97,22 @@ namespace trial
 
                 using (SqlConnection cnn = new SqlConnection(conn))
                 {
-                    cnn.Open();
-                    string day = DateTime.Now.ToString("M/d/yyyy");
-                    string quer1 = " INSERT INTO NET (name , netdate) VALUES (@name , @day)";
-                    SqlCommand command = new SqlCommand(quer1, cnn);
 
-                    command.Parameters.AddWithValue("@name", textBox1.Text);
-                    command.Parameters.AddWithValue("@day", day);
-                    command.ExecuteNonQuery();
+                    if (!string.IsNullOrEmpty(textBox1.Text))
+                    {
+                        cnn.Open();
+                        string day = DateTime.Now.ToString("M/d/yyyy");
+                        string quer1 = " INSERT INTO NET (name , netdate) VALUES (@name , @day)";
+                        SqlCommand command = new SqlCommand(quer1, cnn);
 
+                        command.Parameters.AddWithValue("@name", textBox1.Text);
+                        command.Parameters.AddWithValue("@day", day);
+                        command.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        MessageBox.Show(" fill up the ff.");
+                    }
 
 
                     /*
@@ -133,6 +142,10 @@ namespace trial
             {
                 MessageBox.Show("Error ");
             }
+
+
+
+
         }
 
 
@@ -179,15 +192,17 @@ namespace trial
 
 
 
-                    //  INSERT INTO arvhivetable(id, name, netdate)SELECT* FROM NET
-                    // WHERE id = 6;
-
-                    // DELETE FROM arvhivetable
-                    // WHERE id = 6;
-
 
                     /*
                      
+                    
+                      INSERT INTO arvhivetable(id, name, netdate)SELECT* FROM NET
+                     WHERE id = 6;
+
+                     DELETE FROM arvhivetable
+                    WHERE id = 6;
+
+
                 string sql = @"DELETE*FROM          where id = 1";
                 SqlCommand cmd = new SqlCommand(sql, cnn);
                 cmd.ExecuteNonQuery();
@@ -239,6 +254,242 @@ namespace trial
                 MessageBox.Show("Error ");
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.EditMode = DataGridViewEditMode.EditOnEnter;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView3.Show();
+            search();
+        }
+
+        public void search()
+        {
+            using (SqlConnection cn = new SqlConnection(conn))
+            {
+
+
+                try
+                {
+
+                    cn.Open();
+
+                    string st = "select * from NET WHERE id  = '" + textBox3.Text + "' ";
+
+
+                    SqlDataAdapter adapt = new SqlDataAdapter(st, cn);
+                    SqlCommand command = new SqlCommand();
+
+
+
+                    command.CommandText = st;
+                    command.Parameters.Clear();
+                    DataTable table = new DataTable();
+                    adapt.Fill(table);
+
+                    dataGridView3.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dataGridView3.DataSource = table;
+
+
+
+                }
+                catch (Exception)
+                {
+
+                    //MessageBox.Show(" user name or password incorect");
+                    MessageBox.Show("error", "error", MessageBoxButtons.OK);
+
+
+
+
+                }
+                finally     // to close the connection
+                {
+                    cn.Close();
+
+                } // to close the connection
+
+
+            }
+
+        }
+
+
+
+
+        public void lb()
+        {
+            SqlConnection sqlc = new SqlConnection(conn);
+            try
+            {
+                string str = "select netdate from NET";
+                SqlCommand cmd = new SqlCommand(str, sqlc);
+                cmd.CommandText = str;
+                sqlc.Open();
+                SqlDataReader drd = cmd.ExecuteReader();
+                while (drd.Read())
+                {
+                    comboBox1.Items.Add(drd["netdate"].ToString());
+
+                    /*  comboBox1.ValueMember = drd["product_name"].ToString();
+                      comboBox1.DisplayMember = drd["product_name"].ToString();
+                     */
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error ");
+            }
+
+            finally
+            {
+                sqlc.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+
+        public void login()
+        {
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+
+
+                try
+                {
+                 
+
+                    if (string.IsNullOrEmpty(textBox4.Text) ||
+                                string.IsNullOrEmpty(textBox5.Text))
+                    {
+                        if (textBox4.Text == "")
+                        {
+                            MessageBox.Show(" Enter your user name ");
+
+                        }
+                        else if (textBox5.Text == "")
+                        {
+                            MessageBox.Show(" Enter your password ");
+                        }
+                    }
+                    else ///if (!string.IsNullOrEmpty(textBox1_username.Text) ||
+                        // !string.IsNullOrEmpty(textBox2_password.Text))
+                    {
+
+                        // Open the connection
+
+
+                       
+
+                        //   uc.uid = "SELECT * FROM users WHERE user_id = '1'";
+                        //     MessageBox.Show(uc.uid);
+                        connection.Open();
+
+                        // string quer = "SELECT * FROM users WHERE user_name = '" + txtuser + "' AND password = ' " + txtpass + "'";
+
+                        string quer = "  SELECT user_name ,password , user_type FROM users WHERE user_name = '" +textBox4.Text + "'  AND password =  '" + textBox5.Text + "' ";
+
+                        SqlCommand command = new SqlCommand();
+                        command = new SqlCommand(quer, connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        command.ExecuteNonQuery();
+
+
+                        string quer1 = "  SELECT user_type FROM users WHERE user_name = '" + textBox4.Text + "'  AND password =  '" + textBox5.Text + "' ";
+                        SqlCommand command1 = new SqlCommand();
+                         command1 = new SqlCommand(quer1, connection);
+                         SqlDataReader reader1 = command1.ExecuteReader();
+                        command1.ExecuteNonQuery();
+                        string n = " " + reader[2];
+
+                        if (reader.Read())
+                        {
+                           
+                            if (reader1.Read().Equals("manager"))
+                            {
+                                MessageBox.Show("MANAGER");
+                            }
+                            else if (reader1.Read().Equals("cashier"))
+                            {
+                                MessageBox.Show("CASHIER");
+                              
+                                //     hm1.Show();
+                                //     this.Hide();
+                            }
+
+                            //   }
+                            //    else
+                            //     {
+                            //        MessageBox.Show(" user name or password incorect");
+                            //     }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show(" user name or password incorect");
+                        }
+
+
+
+                        /*
+                                              string staff = ("SELECT user_type  FROM user where user_type = admin");
+                                               SqlCommand command1 = new SqlCommand();
+                                               command1 = new SqlCommand(staff);
+                                               SqlDataReader reader1 = command1.ExecuteReader();
+
+
+
+                                               string staffs = ("SELECT user_type  FROM user where user_type = cashier");
+                                               SqlCommand command2 = new SqlCommand();
+                                               command1 = new SqlCommand(staffs);
+                                               SqlDataReader reader2 = command1.ExecuteReader();
+
+                                           */
+
+
+                        // else if (reader.Read() && reader2.Read())
+                        //  {
+                        //       Home_window hm1 = new Home_window();
+                        //       this.Hide();
+                        //}
+                        //   } // using connection
+                    }
+
+
+                }
+                catch (Exception)
+                {
+
+                    //MessageBox.Show(" user name or password incorect");
+                    MessageBox.Show("error", "error", MessageBoxButtons.OK);
+
+
+
+
+                }
+                finally     // to close the connection
+                {
+                    connection.Close();
+
+                } // to close the connection
+
+
+            }// using connection
+
+        }
+
+        
+
+
+
 
 
     }
